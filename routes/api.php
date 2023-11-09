5f71c7fb4845c8a18a26d33aaddf9127266c99fd
Saturnnnnnnn <hamza.shermatov0504@gmail.com>
2023-11-09 21:04:28 +0300

    first commit

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+use App\Http\Controllers\API\PassportAuthController;
+ 
+Route::post('register', [PassportAuthController::class, 'register']);
+Route::post('login', [PassportAuthController::class, 'login']);
+  
+Route::middleware('auth:api')->group(function () {
+    Route::get('get-user', [PassportAuthController::class, 'userInfo']);
+});
